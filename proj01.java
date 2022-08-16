d3c41065ddbde55b2c9214bb2f09050f0ed5c663
arfazhussain <arfazhussain@users.noreply.github.com>
2022-08-15 23:38:23 -0700

    Aug15

---
+++ proj01.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class proj01 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame Tutorial");
+        frame.setDefaultCloseOperation(operation);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+        
+    }
+}
